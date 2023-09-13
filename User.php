6edb4304aa0fad90e8bba87ae48fd3b0a6d7f51a
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-09-13 14:12:37 +0600

    Update User.php

--- User.php
+++ User.php
@@ -31,3 +31,95 @@ class User extends Model
     }
 
 }
+
+
+Model::creating(function($model){});
+Model::updated(function($model){});
+Model::updating(function($model){});
+Model::deleted(function($model){});
+Model::deleting(function($model){});
+Model::saving(function($model){});
+Model::saved(function($model){});
+
+
+public static function create(array $attributes = [])
+{
+    $model = new static($attributes);
+    $model->save();
+    return $model;
+}
+
+public static function boot()
+{
+    parent::boot();
+
+    // covers created and saved events, as create() method triggers save()
+    static::saved(function($model) {
+        // do xyz
+    });
+}
+
+public static function boot()
+{
+    parent::boot();
+
+    static::created(function($model) {
+        // do xyz
+    });
+
+    static::saved(function($model) {
+        // do xyz
+    });
+}
+
+
+
+use App\Observers\NewsObserver;
+use App\Observers\FileObserver;
+
+class AppServiceProvider extends ServiceProvider
+{
+/**
+ * Bootstrap any application services.
+ *
+ * @return void
+ */
+public function boot()
+{
+    News::observe(NewsObserver::class);
+    File::observe(FileObserver::class);
+}
+
+namespace App\Observers;
+use App\News;
+
+class NewsObserver
+{
+/**
+ * Listen to the News updated event.
+ *
+ * @param  \App\News $item
+ * @return void
+ */
+public function updating(News $item)
+{
+    $publish_at = request()->input('publish_at');
+    $item->publish_at = $publish_at ? $publish_at : $item->publish_at;
+    dd($item);
+}
+
+protected static function boot()
+{
+    parent::boot();
+
+    static::updated(function ($model) {
+        $collector = resolve('touristsCollector');
+        $collector->updated[] = $model;
+    });
+}
+
+
+
+
+
+
